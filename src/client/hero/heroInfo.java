1705afc1489d1e055d204306af2b507c36af9754
nimamoradi <ni_moradi@yahoo.com>
2019-02-12 11:01:31 +0330

    sleepCount

--- src/client/hero/heroInfo.java
+++ src/client/hero/heroInfo.java
@@ -1,6 +1,9 @@
 package client.hero;
 
+import client.model.AbilityName;
 import client.model.Cell;
+import client.model.Hero;
+import client.model.HeroName;
 
 public class heroInfo {
     private int heroId;
@@ -39,4 +42,38 @@ public class heroInfo {
     public void setSleepTurnCount(int sleepTurnCount) {
         this.sleepTurnCount = sleepTurnCount;
     }
+
+    public int sleepCount(AbilityName abilityName) {
+        switch (abilityName) {
+            case SENTRY_RAY:
+                return 5;
+            case SENTRY_ATTACK:
+                return 1;
+            case SENTRY_DODGE:
+                return 7;
+
+            case HEALER_HEAL:
+                return 3;
+            case HEALER_ATTACK:
+                return 1;
+            case HEALER_DODGE:
+                return 6;
+
+            case BLASTER_BOMB:
+                return 4;
+            case BLASTER_DODGE:
+                return 6;
+            case BLASTER_ATTACK:
+                return 1;
+
+            case GUARDIAN_DODGE:
+                return 8;
+            case GUARDIAN_FORTIFY:
+                return 7;
+            case GUARDIAN_ATTACK:
+                return 1;
+        }
+        return 0;
+    }
+
 }
